fc097f96df59ba9e930e83907b6d534695233d3c
mriskiaziz <mriski.119140160@student.itera.ac.id>
2023-11-10 23:08:59 +0700

    update middlewares 03

--- middlewares/withAuth.js
+++ middlewares/withAuth.js
@@ -1,16 +1,16 @@
 import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
+const secret = process.env.NEXTAUTH_SECRET;
+
 export default function withAuth(middleware, requireAuth = []) {
   return async (req, next) => {
     const pathname = req.nextUrl.pathname;
 
     if (requireAuth.includes(pathname)) {
-      const token = await getToken({
-        req,
-        secret: process.env.NEXTAUTH_SECRET,
-      });
+      const token = await getToken({ req, secret });
       console.log(token);
+      console.log(secret);
 
       if (!token) {
         const url = new URL("/login", req.url);
