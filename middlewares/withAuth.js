1a95d3d26f94d88a009f1f19cd15cc2f09e01938
mriskiaziz <mriski.119140160@student.itera.ac.id>
2024-06-10 17:29:36 +0700

    middleware

--- middlewares/withAuth.js
+++ middlewares/withAuth.js
@@ -1,14 +1,14 @@
 import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
-const secret = process.env.NEXTAUTH_SECRET;
+// const secret = process.env.NEXTAUTH_SECRET;
 
 export default function withAuth(middleware, requireAuth = []) {
   return async (req, next) => {
     const pathname = req.nextUrl.pathname;
 
     if (pathname == "/login") {
-      const token = await getToken({ req, secret });
+      const token = await getToken({ req});
 
       if (token) {
         console.log(token);
@@ -20,7 +20,7 @@ export default function withAuth(middleware, requireAuth = []) {
     }
 
     if (requireAuth.includes(pathname)) {
-      const token = await getToken({ req, secret });
+      const token = await getToken({ req });
       console.log(token);
 
       if (!token) {
