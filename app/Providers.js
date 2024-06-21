ab286eb8de26cf2f6c21b89917d8c3f05a7d9d4f
mriskiaziz <mriski.119140160@student.itera.ac.id>
2024-06-21 16:22:41 +0700

    edit providers

--- app/Providers.js
+++ app/Providers.js
@@ -1,7 +1,18 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { useEffect, useState } from "react";
 
 export const AuthProvider = ({ session, children }) => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
   return <SessionProvider session={session} >{children}</SessionProvider>;
 };
